683db35cd919e1fe6f4994607c0b077eb00132bc
Brandon Wright <brandonfullstack@gmail.com>
2023-07-03 23:40:41 -0400

    WIP: More business logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,38 +2,32 @@
 
 function BankAccount() {
   this.transactions = {};
-  this.balance = {};
+  this.balance = 0;
   this.currentId = 0;
 }
 
 BankAccount.prototype.addTransaction = function(transaction) {
   transaction.id = this.assignId();
   this.transactions[transaction.id] = transaction;
+  this.balance += transaction.amount;
 };
 
 BankAccount.prototype.assignId = function() {
   this.current += 1;
   return this.currentId;
-};
-
-BankAccount.prototype.calculateBalance = function() {
-  this.currentBalance = this.amount;
-  return this.currentBalance;
-} 
+}; 
 
 // Business Logic for Transactions --------
-
-intialBalance = document.querySelector("input#intial-balance").value
 
 function Transaction(description, amount, date) {
   this.description = description;
   this.amount = amount;
-  this.date = date; 
+  this.date = date;
 }
-
-
 
 Transaction.prototype.lineItem = function() {
   return this.date + " " + this.description + " " + this.amount + " " + this.balance;
 };
+
+// User Interface Logic --------------------
 
